34d4c4531c74d29bcb2ac35e45fe70d91f4f0e4f
Miles Matthias <miles.matthias@gmail.com>
2016-08-01 17:07:46 +0000

    wip

--- lib/uber/api/oauth.rb
+++ lib/uber/api/oauth.rb
@@ -1,5 +1,6 @@
 require 'uber/arguments'
 require 'uber/api_request'
+require 'oauth2'
 
 module Uber
   module API
@@ -9,21 +10,20 @@ module Uber
       BASE_TOKEN_URL  = "https://login.uber.com/oauth/v2/token"
       BASE_REVOKE_URL = "https://login.uber.com/oauth/revoke"
 
-      def oauth_url(opts={})
-        opts[:client_id]     ||= client_id
-        opts[:redirect_uri]  ||= redirect_uri
-        opts[:response_type] ||= "code"
+      OAUTH_URLS = {
+        :site => "https://login.uber.com",
+        :authorize_url => "/oauth/v2/authorize",
+        :token_url => "/oauth/v2/token"
+      }
 
-        [BASE_AUTH_URL, Faraday::FlatParamsEncoder.encode(opts)].join("?")
+      def oauth_url
+        oauth_client = OAuth2::Client.new(client_id, client_secret, OAUTH_URLS)
+        oauth_client.authorize_url(redirect_uri: redirect_uri)
       end
 
-      def oauth_access_token(opts)
-        opts[:grant_type]   ||= "authorization_code"
-        opts[:client_id]    ||= client_id
-        opts[:redirect_uri] ||= redirect_uri
-
-        request = Uber::ApiRequest.new(self, :get, BASE_TOKEN_URL, opts)
-        request.perform
+      def oauth_access_token(code)
+        oauth_client = OAuth2::Client.new(client_id, client_secret, OAUTH_URLS)
+        oauth_client.auth_code.get_token(code)
       end
 
       def refresh_oauth_token(opts)
